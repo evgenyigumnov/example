5e2e90ecaaf269469f72a456cdba677fa80d61e7
evgenyigumnov <igumnov@gmail.com>
2015-05-27 23:53:28 +0600

    login page

--- src/main/java/com/igumnov/common/example/App.java
+++ src/main/java/com/igumnov/common/example/App.java
@@ -3,6 +3,8 @@ package com.igumnov.common.example;
 import com.igumnov.common.ORM;
 import com.igumnov.common.WebServer;
 
+import java.util.Date;
+
 public class App {
 
     public static void main(String[] args) throws Exception {
@@ -10,6 +12,23 @@ public class App {
         ORM.connectionPool("org.h2.Driver", "jdbc:h2:mem:test", "SA", "", 10, 30);
         ORM.applyDDL("sql");
         WebServer.init("localhost", 8181);
+
+        WebServer.security("/login", "/login", "/logout");
+        WebServer.addRestrictRule("/*", new String[]{"user_role"});
+        WebServer.addAllowRule("/static/*");
+        WebServer.addUser("demo", "demo", new String[]{"user_role"});
+
+        WebServer.addTemplates("pages");
+        WebServer.addController("/", (request, model) -> {
+            model.put("time", new Date());
+            return "index";
+        });
+        WebServer.addController("/login", (request, model) -> {
+            return "login";
+        });
+
+
+
         WebServer.start();
 
     }
